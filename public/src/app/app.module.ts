33bebe7f48191b59ff5c68bfbb2f19f9d2c505f7
thanhdatvo <thanhdat.vo.email@gmail.com>
2017-01-30 09:41:37 +0700

    create routes, initial rest service

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -2,19 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { LocationStrategy, PathLocationStrategy, APP_BASE_HREF } from '@angular/common';
+
+
+import { routing } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { RecommendationsComponent } from './components/recommendations/recommendations.component';
+import { ChallengesComponent } from './components/challenges/challenges.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
+import { ProfilesComponent } from './components/profiles/profiles.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { HeaderComponent } from './components-shared/header/header.component';
+import { FooterComponent } from './components-shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    RecommendationsComponent,
+    ChallengesComponent,
+    NotificationsComponent,
+    ProfilesComponent,
+    CategoriesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: PathLocationStrategy },
+    { provide: APP_BASE_HREF, useValue: '/' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
