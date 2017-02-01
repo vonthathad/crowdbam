1832a7d507d00e025d728d7a3336f6a55da59b20
thanhdatvo <thanhdat.vo.email@gmail.com>
2017-02-01 10:14:08 +0700

    +f

--- public/src/app/services/user.service.ts
+++ public/src/app/services/user.service.ts
@@ -57,7 +57,7 @@ export class UserService {
             headers: headers
         });
     }
-    checkUser(): boolean {
+    checkLoggedInStatus(): boolean {
         if (this.user) return true;
         return false;
     }
